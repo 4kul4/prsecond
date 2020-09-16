895f1e9d07dedef606c12aeb7bc14ca9ef8f79f7
4kul4 <70626971+4kul4@users.noreply.github.com>
2020-09-16 09:44:07 +0300

    Add files via upload

---
+++ pr3(2)/src/second/TestBall.java
@@ -0,0 +1,10 @@
+package second;
+
+public class TestBall {
+    public static void main(String[] args){
+        Ball m=new Ball(17,36);
+        System.out.println(m);
+        m.move(13,-14);
+        System.out.println(m);
+    }
+}
